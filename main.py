3c20f445b7e1d6af2a3fe6eb91ed34da4338ed31
Sreenketh Madgula <sreeniketh.madgula@gmail.com>
2022-06-17 12:40:44 +0530

    hugging face changes to be made

--- main.py
+++ main.py
@@ -22,22 +22,28 @@ def summarize(file1 = "frontend.html"):
     youtube_id_match_object = re.search(r'.*=(.*)$',youtube_link)
     youtube_id = youtube_id_match_object.groups(0)[0]
     print(youtube_id)
-    text_dictionary_list = YouTubeTranscriptApi.get_transcript(youtube_id)
-    transcript_text =""
-    for i in text_dictionary_list:
-        transcript_text+=' '+i['text']
-    transcript_file = open("transcript.txt","w")
-    transcript_file.write(transcript_text)
+    try:
+        text_dictionary_list = YouTubeTranscriptApi.get_transcript(youtube_id)
+        transcript_text =""
+        for i in text_dictionary_list:
+            transcript_text+=' '+i['text']
+        transcript_file = open("transcript.txt","w")
+        transcript_file.write(transcript_text)
 
-    classifier = pipeline("summarization")
-    summary_dict = classifier(transcript_text)
+        classifier = pipeline("summarization")
+        summary_dict = classifier(transcript_text)
 
-    print(summary_dict)
-    summary = summary_dict[0]["summary_text"]
-    print(summary)
+        print(summary_dict)
+        summary = summary_dict[0]["summary_text"]
+        print(summary)
 
-    summary_file = open("summary.txt","w")
-    summary_file.write(summary)
+        summary_file = open("summary.txt","w")
+        summary_file.write(summary)
+    # except youtube_transcript_api._errors.TranscriptsDisabled:
+    except TranscriptsDisabled:
+        print(TranscriptsDisabled.CAUSE_MESSAGE)
+        
+
 
 
 
