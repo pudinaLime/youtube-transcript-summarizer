6d26258e366504518d19612d7f9eff9b026b1b2a
Sreenketh Madgula <sreeniketh.madgula@gmail.com>
2022-06-21 13:15:13 +0530

    hugging face; changes to be made; errors handled

--- main.py
+++ main.py
@@ -7,30 +7,34 @@ from transformers import pipeline
 app = Bottle()
 # app_path = 
 
-
-@app.route('/hello')
-def hello():
-    return "Hello World!"
-
-@app.route('/')
-def home(filename = "frontend.html"):
-    return static_file(filename, root='./')
-
-@app.post('/')
-def summarize(file1 = "frontend.html"):
-    youtube_link = request.forms.get('youtube_link')
+def get_youtube_ID(youtube_link):
     youtube_id_match_object = re.search(r'.*=(.*)$',youtube_link)
     youtube_id = youtube_id_match_object.groups(0)[0]
-    print(youtube_id)
+    return youtube_id
+
+
+def get_transcript_text(youtube_id):
     text_dictionary_list = YouTubeTranscriptApi.get_transcript(youtube_id)
-    transcript_text =""
+    transcript_text = ''
     for i in text_dictionary_list:
-        transcript_text+=' '+i['text']
+        new_line = i['text']
+        
+        transcript_text+=new_line + ' '
+    
+
     transcript_file = open("transcript.txt","w")
     transcript_file.write(transcript_text)
+    return transcript_text
 
+def test_working(youtube_link):
+    youtube_id = get_youtube_ID(youtube_link)
+    text = get_transcript_text(youtube_id)
+
+
+
+def get_summary(text):
     classifier = pipeline("summarization")
-    summary_dict = classifier(transcript_text)
+    summary_dict = classifier(text)
 
     print(summary_dict)
     summary = summary_dict[0]["summary_text"]
@@ -38,12 +42,32 @@ def summarize(file1 = "frontend.html"):
 
     summary_file = open("summary.txt","w")
     summary_file.write(summary)
+    return summary
 
+    
+@app.error(500)
+def error500(error):
+    return template('error500')
+@app.route('/')
+def home(filename = "frontend.html"):
+    return static_file(filename, root='./')
 
+@app.route('/',method='POST')
+def summarize(file1 = "frontend.html"):
+    youtube_link = request.forms.get('youtube_link')
 
-    # print(text_dictionary_list)
-    # print(transcript_text) 
+    transcript_file = request.forms.get('file')
+
+    youtube_id = get_youtube_ID(youtube_link)
+
+    transcript_text = get_transcript_text(youtube_id)
+    
+    transcript_text = transcript_text[:1000]
+
+    summary = get_summary(transcript_text)
+
     return static_file(file1, root = './')
 
 
-run(app, host='localhost', port=8080, reloader=True)
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080, reloader=True)
---
+++ views/error500.tpl
@@ -0,0 +1,11 @@
+<head>
+    <title>Error Page</title>
+</head>
+
+<body>
+
+    <h1>Error 500 occured : Internal Server Error</h1>
+    <form action="http://localhost:8080/" enctype="multipart/form-data">
+    <input type="submit" value="Go Back"/>
+
+</body>
